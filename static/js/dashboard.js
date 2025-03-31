8e80d43a68343b02bc6bd02a0652be89053a9856
lilblueyes <101664783+lilblueyes@users.noreply.github.com>
2025-03-31 14:55:24 +0200

    Enhance username styling font

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,5 +1,5 @@
 window.socket = io({
-    withCredentials: true
+  withCredentials: true
 });
 socket.emit('request_game_info');
 
@@ -7,12 +7,21 @@ const teamSelect = document.getElementById('team');
 const joinBtn = document.getElementById('joinBtn');
 
 socket.on('connect', () => {
-    console.log('Connecté au serveur');
+  console.log('Connecté au serveur');
 });
 
 // Gestionnaire pour rejoindre une partie
 joinBtn.addEventListener('click', () => {
-    const team = parseInt(teamSelect.value);
+  const team = parseInt(teamSelect.value);
 
-    socket.emit('join_game', { team });
+  socket.emit('join_game', { team });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const usernameSpan = document.getElementById('username');
+  const username = usernameSpan.textContent;
+
+  const capitalizedUsername =
+    username.charAt(0).toUpperCase() + username.slice(1);
+  usernameSpan.textContent = capitalizedUsername;
 });
