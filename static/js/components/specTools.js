a98936e9b7c931c556a252a5eba0855d47b8c993
Guillaume Seimandi <guillaume.seimandi@hotmail.com>
2025-04-12 13:51:48 +0200

    Suppression de l'utilitaire pour spectateur

--- static/js/components/specTools.js
+++
@@ -1,6 +0,0 @@
-const specToolsSection = document.getElementById('spec-tools-section');
-
-socket.on('load-spec-tools', () => {
-    console.log("Vous êtes un spec")
-    specToolsSection.className = specToolsSection.className.replace(/\bd-none\b/g, 'd-flex');
-});
